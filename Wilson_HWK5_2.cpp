80a2c0044602a33d506e6e4827f78bd5196cd33e
Zoe Wilson <zoewilson169@gmail.com>
2016-02-18 20:11:54 -0500

    Added comments to Wilson_HWK5_2.cpp

--- Wilson_HWK5_2.cpp
+++ Wilson_HWK5_2.cpp
@@ -8,11 +8,12 @@
 #include <vector>
 #include <sstream>
 
-
+// Deletes any repeats in the array
 std::vector<char> delete_repeats(char a[], int& count){
     std::vector<char> seen;
     seen.push_back(a[0]); // We know we want the first element
 
+    // Store non-repeats into new array
     for (int i = 1; i < count; i++) {
         bool found = false;
         for (int j = 0; j < seen.size(); j++) {
@@ -31,12 +32,14 @@ std::vector<char> delete_repeats(char a[], int& count){
 int main() {
     char array[] = {'a', 'b', 'c', 'd', 'a', 'x', 'x', 'h'};
     int count = 8;
+
+    // Send array and count to function
     std::vector<char> new_array = delete_repeats(array, count);
     
+    // Printing final results
     std::cout << "The original array was: {'a', 'b', 'c', 'd', 'a', 'x', 'x', 'h'} with a count of 8." 
               << std::endl;
     std::cout << "The new array is: ";     
-    
     for (int i = 0; i < new_array.size(); i++) {
         std::cout << new_array[i];
     }  
