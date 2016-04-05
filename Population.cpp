9c393cfc13717ff874dd1fe4038626a7ba70fc8e
Zoe Wilson <zoewilson789@gmail.com>
2016-04-05 16:56:20 -0600

    Cleaning up code

--- Population.cpp
+++ Population.cpp
@@ -4,81 +4,117 @@
  * Population problem #3
  */
 
+
 #include <iostream>
 
-class Population {
-   private:
-      float population;
-      float birth_rate;
-      float death_rate;
 
-   public:
-      // Constructor
-      Population(float _p, float _br, float _dr) : population(_p), birth_rate(_br), death_rate(_dr) {}
-      // Destructor
-      Population(const Population& orig) : population(orig.population), birth_rate(orig.birth_rate),
-         death_rate(orig.death_rate) {}
-      // Copy constructor
-      ~Population() {}
+/* Population */
+class Population {
+    
+  private:
+    
+    /* The population total */
+    float population;
+    
+    /* The birth rate */
+    float birth_rate;
+    
+    /* The death rate */
+    float death_rate;
+
+  public:
+   
+    /* Constructor */
+    Population(float _p, float _br, float _dr) 
+        : population(_p), birth_rate(_br), death_rate(_dr) {}
+        
+    /* Destructor */
+    Population(const Population& orig) 
+        : population(orig.population), birth_rate(orig.birth_rate), death_rate(orig.death_rate) {}
+    
+    /* Copy constructor */
+    ~Population() {}
       
-      // Getters
-      void set_population(float new_pop) { population = new_pop; }
-      float get_population() const { return population; }
-      float get_birth_rate() const { return birth_rate; }
-      float get_death_rate() const { return death_rate; }
+    /* Sets the population */
+    void set_population(float new_pop) { 
+        population = new_pop;
+    }
+    
+    /* Gets the population */
+    float get_population() const { 
+        return population;
+    }
+    
+    /* Gets the birth rate */
+    float get_birth_rate() const { 
+        return birth_rate;
+    }
+    
+    /* Gets the death rate */
+    float get_death_rate() const { 
+        return death_rate; 
+    }
+    
 };
 
-// Operator overload on <<
-std::ostream& operator << (std::ostream& out, Population& pop) {
-   pop.set_population(pop.get_population() + (pop.get_population() * (pop.get_birth_rate()/100))
-      + (pop.get_population() * (pop.get_death_rate()/100)));
+
+/* Operator overload for << */
+std::ostream& operator << (std::ostream& out, Population& population) {
+    population.set_population(population.get_population() 
+                              + (population.get_population() * (population.get_birth_rate() / 100))
+                              + (population.get_population() * (population.get_death_rate() / 100)));
    
-   //Convert to int - truncates float because you can't have part of a person
-   int new_pop = pop.get_population(); 
-   out << new_pop << std::endl;
+   // Convert to an integer by truncating float: cannot have a fraction of a person
+   int new_population = population.get_population(); 
+   out << new_population << std::endl;
+   
    return out;
 }
 
+
+/* Main */
 int main() {
-   float population = 0;
-   float birth_rate = 0;
-   float death_rate = 0;
-   int years = 0;
+    float population = 0;
+    float birth_rate = 0;
+    float death_rate = 0;
+    int years = 0;
 
-   while(true) {
-      std::cout << "Enter the beginning population: ";
-      std::cin >> population;
+    while (true) {
+        std::cout << "Enter the beginning population: ";
+        std::cin >> population;
       
-      // Population must be greater than 2
-      if(population < 2) std::cout << "Please enter the beginning value of your population (greater than 2): ";
-      else break;
-   }
+        // Population must be greater than 2
+        if (population >= 2) {
+            break;
+        }
+        std::cout << "Please enter the beginning value of your population (greater than 2): ";
+    }
 
-   std::cout << "Enter the birth rate (%): ";
-   std::cin >> birth_rate;
+    std::cout << "Enter the birth rate (%): ";
+    std::cin >> birth_rate;
 
-   std::cout << "Enter the death rate (%): ";
-   std::cin >> death_rate;
+    std::cout << "Enter the death rate (%): ";
+    std::cin >> death_rate;
 
-   if(death_rate > 0) {
-       death_rate *= -1;
-   }
+    if (death_rate > 0) {
+        death_rate *= -1;
+    }
 
-   // Creating population object
-   Population myPopulation(population, birth_rate, death_rate);
+    // Creating population object
+    Population myPopulation(population, birth_rate, death_rate);
 
-   std::cout << "Enter how many years you would like to pass: ";
-   std::cin >> years;
+    std::cout << "Enter how many years you would like to pass: ";
+    std::cin >> years;
 
-   // Printing Results
-   for(int i = 0; i < years; ++i) {
-      std::cout << "----------------" << std::endl;
-      std::cout << "Year: " << i + 1 << std::endl;
-      std::cout << "Population: " << myPopulation;
-      if(myPopulation.get_population() < 1) {
-         std::cout << "Your population died at year " << i + 1 << std::endl;
-         break;
-      }
-   }
-   return 0;
+    // Printing Results
+    for (int i = 0; i < years; ++i) {
+        std::cout << "----------------" << std::endl;
+        std::cout << "Year: " << i + 1 << std::endl;
+        std::cout << "Population: " << myPopulation;
+        if (myPopulation.get_population() < 1) {
+            std::cout << "Your population died at year " << i + 1 << std::endl;
+            break;
+        }
+    }
+    return 0;
 }
